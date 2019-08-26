816e6387f92d7b32a8b94926856f3d8c4859a79f
anamontiaga <amontiaga@gmail.com>
2019-08-26 18:01:41 +0200

    last changes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,5 +1,5 @@
 "use strict";
-// Definimos las constantes con los elementos del DOM que vamos a necesitar
+
 const cardsBtns = document.querySelectorAll(".header_form--option");
 const cardsList = document.querySelector(".cards_container--list");
 const searchBtn = document.querySelector(".header_form--btn");
@@ -13,7 +13,6 @@ function setLocalStorage() {
   localStorage.setItem("number of cards", JSON.stringify(btnValue));
 }
 
-// Buscamos las cartas llamando a la API
 function getCardInfo() {
   for (const cardBtn of cardsBtns) {
     if (cardBtn.checked === true) {
@@ -31,23 +30,22 @@ function getCardInfo() {
 function paintCards(ev) {
   ev.preventDefault();
   getCardInfo();
-  cardsList.innerHTML = ""; // te lo pinta en blanco y no me repite las búsquedas y refresca
+  cardsList.innerHTML = "";
   for (let i = 0; i < cardsArray.length; i++) {
     cardsList.innerHTML += `<li class="cards_container--list--item">
         <img src="${cardsArray[i].image}" alt="${cardsArray[i].image}" class="cards_container--list--image hidden">
         <img src="${cardAdalab}" alt="Adalab" class="cards_container--list--image--adalab"></li>`;
   }
+  // 1º He intentado ejecutar esta parte debajo de la función flipCards() pero me "rompe" el juego.
   const cardsItems = document.querySelectorAll(".cards_container--list--item");
   for (const item of cardsItems) {
     item.addEventListener("click", flipCards);
   }
-  // 1º ¿Por qué flipCards no se ejecuta con el addEventListener abajo de la función?
 }
 
 searchBtn.addEventListener("click", paintCards);
 
-// 2º tengo que dar dos veces al botón para que me pinte las cartas
-
+// 2º Tengo que dar dos veces al botón para que me pinte las cartas. Algo falla.
 function flipCards(event) {
   const cardGame = event.currentTarget.querySelector(".cards_container--list--image");
   const cardDefault = event.currentTarget.querySelector(".cards_container--list--image--adalab");
@@ -55,6 +53,7 @@ function flipCards(event) {
   cardDefault.classList.toggle("hidden");
 }
 
+// 3º Esta función no funciona. Se guardan los datos en LS pero no los coge para volverlos a pintar
 function catchData() {
   const catchInfo = JSON.parse(localStorage.getItem("number of cards"));
   if (catchInfo !== null) {
